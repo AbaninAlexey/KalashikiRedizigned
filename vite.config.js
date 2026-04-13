279090c053932eb8de888a3a4c3a4150f1ac9981
Abanin Alexey <abanin_alexey@mail.ru>
2026-04-13 22:52:10 +0300

    исправлен роутинг для ghpages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  base: '/KalashikiRedizigned/',
   root: '.',
   plugins: [react(), svgr()],
   resolve: {
